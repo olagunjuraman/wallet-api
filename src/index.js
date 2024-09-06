e72b88d8fba29106c94b8238e38768d93e2c384d
raman <raman@figo.africa>
2024-09-06 16:07:12 +0100

    added-test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require('../wallet');
 
 const port = process.env.PORT || 3000;
 
-app.get('/health', (req, res) => {
+app.get('/api/health', (req, res) => {
     res.status(200).json({ status: 'OK', message: 'Wallet API is healthy' });
 });
 
